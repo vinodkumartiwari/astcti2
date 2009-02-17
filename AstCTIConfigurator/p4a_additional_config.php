5534dacd58751235bce1dfcc6b0ce4c8ed82a348
fabrizio.balliano <fabrizio.balliano@26fa2d7c-d06c-11dd-a3c7-fda619b15ae3>
2009-02-17 08:10:41 +0000

    db updated to latest version, remove copyrights from local configuration file

--- AstCTIConfigurator/p4a_additional_config.php
+++ AstCTIConfigurator/p4a_additional_config.php
@@ -1,24 +1,5 @@
 <?php
 /**
- * This file is part of AstCTIConfigurator.
- * 
- * AstCTIConfigurator is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * AstCTIConfigurator is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * @author Fabrizio Balliano <fabrizio.balliano@crealabs.it>
- * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+ * Put here your additional P4A configurations.
  */
-	
-	// Put here additional P4A configurations.
-	
-?>
+
